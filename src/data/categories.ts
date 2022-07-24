a94fd8fd83d9581b666a79f6e9cc5b0c32eb42bd
LisiQuadros <elisiane.quadros@hotmail.com>
2022-07-23 21:18:25 -0300

    Segundo commit

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -3,7 +3,6 @@ import { Category }from '../types/Category'
 export const categories: Category = {
   food: {title: 'Alimentação', color:'#4A808C', expense: true},
   rent: {title: 'Despesas', color:'#F24949', expense: true},
-  card: {title: 'Despesas', color:'#F24949', expense: true},
   salary: {title: 'Proventos', color:'#0D4A3A', expense: false},
 
 }
