7a449386ba923e0b9b446c7795c691e2d5013b91
LisiQuadros <elisiane.quadros@hotmail.com>
2022-08-07 16:36:54 -0300

    adicionado novas categorias de despesas

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,7 +2,8 @@ import { Category }from '../types/Category'
 
 export const categories: Category = {
   food: {title: 'Alimentação', color:'#4A808C', expense: true},
-  rent: {title: 'Despesas', color:'#F24949', expense: true},
+  rent: {title: 'Transporte', color:'#F24949', expense: true},
+  home: {title: 'Moradia',color:'#F24949', expense: true },
+  card: {title: 'Cartões', color:'#F24949', expense: true},
   salary: {title: 'Proventos', color:'#0D4A3A', expense: false},
-
 }
